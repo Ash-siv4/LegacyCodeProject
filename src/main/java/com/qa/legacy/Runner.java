fa439aa4dc927a4224ae7f60a24c19bce3b9c055
Piers Barber <kochab.96@gmail.com>
2024-01-31 13:49:04 +0000

    Added Database connection

---
+++ src/main/java/com/qa/legacy/Runner.java
@@ -0,0 +1,14 @@
+package com.qa.legacy;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Runner {
+	
+	public static final Logger LOGGER = LogManager.getLogger();
+	
+	public static void main(String[] args) {
+		// Stub
+		LOGGER.info("SO LONG!");
+	}
+}
---
+++ src/main/resources/sql-data.sql
@@ -0,0 +1,9 @@
+INSERT INTO `legacy`.`items` (`item_name`, `item_price`, `item_quantity`) VALUES ('harmonica', 1200.99, 8);
+INSERT INTO `legacy`.`items` (`item_name`, `item_price`, `item_quantity`) VALUES ('wheelie bin', 2.99, 84528);
+INSERT INTO `legacy`.`items` (`item_name`, `item_price`, `item_quantity`) VALUES ('your mother', 0.99, 1);
+INSERT INTO `legacy`.`items` (`item_name`, `item_price`, `item_quantity`) VALUES ('nike airs', 40.99, 74);
+INSERT INTO `legacy`.`cart` (`fk_item_id`) VALUES (3);
+INSERT INTO `legacy`.`cart` (`fk_item_id`) VALUES (1);
+INSERT INTO `legacy`.`cart` (`fk_item_id`) VALUES (2);
+INSERT INTO `legacy`.`cart` (`fk_item_id`) VALUES (2);
+INSERT INTO `legacy`.`cart` (`fk_item_id`) VALUES (4);
